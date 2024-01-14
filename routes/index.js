cbe03171b04c5ceef4fd4c6903ae33dbbaee7c85
Aazarb <zaraba.adil@hotmail.fr>
2024-01-14 23:04:34 +0100

    router, profile, css, topbar

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) =>{
-  res.render('home');
+router.get('/', (req, res) => {
+  res.render('tweets/tweet-list');
 });
 
 module.exports = router;
